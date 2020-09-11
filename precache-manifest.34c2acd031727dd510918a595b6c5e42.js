b4e92852a3facef5dc94c8b6ce4b331e32550817
home <home@homes-MacBook-Pro.local>
2020-09-11 21:18:12 +0530

    Updates

---
+++ precache-manifest.34c2acd031727dd510918a595b6c5e42.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e653e70cc5a1f78a00ffccc14ce09002",
+    "url": "/to-dolist-app/index.html"
+  },
+  {
+    "revision": "fdfb1272c7feba12b185",
+    "url": "/to-dolist-app/static/css/main.b1f82be5.chunk.css"
+  },
+  {
+    "revision": "b0eb4ff8ab35bf026f6d",
+    "url": "/to-dolist-app/static/js/2.ad6cc904.chunk.js"
+  },
+  {
+    "revision": "c0c0a28e3e1b198d368782bdda7f2849",
+    "url": "/to-dolist-app/static/js/2.ad6cc904.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fdfb1272c7feba12b185",
+    "url": "/to-dolist-app/static/js/main.2366f0bb.chunk.js"
+  },
+  {
+    "revision": "1e389970a03054f61940",
+    "url": "/to-dolist-app/static/js/runtime-main.d1acf20d.js"
+  }
+]);
